1098e1c2320ce15365c73ac766689a5982925d88
Zeran Lu <zeranlu0326@gmail.com>
2026-03-05 15:44:24 -0500

    Added folder structure from class build
    in this commit, the build structure from was imported

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { Counter } from './counter.js';
+
+const counter1 = new Counter("#counter-container1");
+const counter2 = new Counter("#counter-container2");
+const randomNewBox = new Counter("#randomBocx");
