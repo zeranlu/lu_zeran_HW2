236a64fc54441366dd6942c085777ad072361043
Zeran Lu <zeranlu0326@gmail.com>
2026-03-19 19:11:29 -0400

    JS: linked new counters as variables in main.js
    in this commit, the html counter skeletons were instantiated with JS in main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ import { Counter } from './counter.js';
 
 const counter1 = new Counter("#counter-container1");
 const counter2 = new Counter("#counter-container2");
-const randomNewBox = new Counter("#randomBocx");
+
+const counter3 = new StepCounter("#counter-container3");
+const counter4 = new StepCounter("#counter-container4");
